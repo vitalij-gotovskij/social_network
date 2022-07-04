9a72aaaca29a338feab456305222cf68d187afd4
Vitalij Gotovskij <vitalij@cdeveloper.eu>
2022-07-04 21:27:25 +0300

    uuid missing import fix

--- social_network/posts/models.py
+++ social_network/posts/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.db import models
 from django.contrib.auth.models import User
 from tinymce import models as tinymce_models
@@ -13,7 +15,7 @@ class Wall(models.Model):
 class Post(models.Model):
     owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     content = tinymce_models.HTMLField(default='')
-    wall = models.ForeignKey(Wall, on_delete=models.SET_NULL, null=True)
+    wall = models.ForeignKey('Wall', on_delete=models.SET_NULL, null=True)
     pin_to_top = models.BooleanField()
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now_add=True)
@@ -32,7 +34,7 @@ class Meta:
 class Like(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for every like')
     post = models.ForeignKey('Post', on_delete=models.CASCADE, null=True)
-    owner = models.ForeignKey('User', on_delete=models.CASCADE, null=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     created_at = models.DateTimeField(auto_now_add=True, help_text='Time of like')
 
     def __str__(self):
