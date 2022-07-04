f706c86e3e8592d44ab2184501afaf820e2d263a
Vitalij Gotovskij <vitalij@cdeveloper.eu>
2022-07-04 20:47:19 +0300

    Model classes registered with Admin Panel

--- social_network/posts/admin.py
+++ social_network/posts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Wall, Post, Like
 
 # Register your models here.
+
+
+admin.site.register(Wall)
+admin.site.register(Post)
+admin.site.register(Like)
