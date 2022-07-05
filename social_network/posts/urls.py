b6720f37ee515bc2f243b543815998f78e2efe32
Benas Jasikonis <benasjasikonis@gmail.com>
2022-07-05 21:14:06 +0300

    Post Detail view added

--- social_network/posts/urls.py
+++ social_network/posts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import PostCreateView, PostListView, index
+from .views import index, PostCreateView, PostListView, PostDetailView
 
 urlpatterns = [
     path('', index, name='index'),
     path('post_list/', PostListView.as_view(), name='post_list'),
     path('create_post/', PostCreateView.as_view(), name='create-post'),
+    path('post_list/<int:pk>', PostDetailView.as_view(), name='post-detail'),
 ]
